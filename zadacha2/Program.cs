0e64eba2047faf115b2ca73e67e368df5424562b
MaksimSukhanov <filmskram@gmail.com>
2023-04-23 22:50:49 +0300

    выполнил дз на 1ый семинар по C#

---
+++ zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+int Max(int arg1,int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result=arg2;
+    if(arg3>result) result=arg3;
+    return result;
+}
+
+int number1 = 23, number2 = 42, number3 = 344;
+
+int max = Max(number1,number2,number3);
+Console.WriteLine(max);

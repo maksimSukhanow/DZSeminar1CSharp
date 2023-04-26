e832c9bf8a2067f64f2153b6adaddf532cecce38
MaksimSukhanov <filmskram@gmail.com>
2023-04-26 23:52:26 +0300

    сделал задачу 15

--- zadacha15/Program.cs
+++ zadacha15/Program.cs
@@ -3,3 +3,11 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 1 | num > 7) Console.WriteLine("В неделе 7 дней");
+else if (num < 6)
+{
+    Console.WriteLine("Сегодня рабочий день");
+}
+else Console.WriteLine("Сегодня выходной");
